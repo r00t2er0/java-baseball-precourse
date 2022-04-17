6db19a73e215544a2d0372a2b884bb8ee95c61a1
hermes9003 <hermes9003@ariatech.kr>
2022-04-17 18:40:12 +0900

    feat: update Player object
    Change function of Player:
    - add constructor
    - add initialize function
    - modify setNumber function
    
    BREAKING CHANGE: after splitting the input string,
    set it to the GameNumber object

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -1,16 +1,25 @@
 package baseball.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
     private GameNumber gameNumber;
 
-    public void setNumber(GameNumber gameNumber) {
-        this.gameNumber = gameNumber;
+    public Player() {
+        this.gameNumber = new GameNumber(new ArrayList<>());
+    }
+
+    public void setNumber(String inputNumber) {
+        String[] number = inputNumber.split("");
+        this.gameNumber.setNumbers(number);
     }
 
     public List<Integer> getNumber() {
         return gameNumber.getNumbers();
     }
 
+    public void initGameNumber() {
+        this.gameNumber = new GameNumber(new ArrayList<>());
+    }
 }
