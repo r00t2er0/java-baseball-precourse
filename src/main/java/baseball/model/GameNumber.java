2cf1e5db9fb264ed8bdf81b9c8dc96e4a80ab195
hermes9003 <hermes9003@ariatech.kr>
2022-04-17 18:17:13 +0900

    feat: add GameNumber object
    Add Object

---
+++ src/main/java/baseball/model/GameNumber.java
@@ -0,0 +1,15 @@
+package baseball.model;
+
+import java.util.List;
+
+public class GameNumber {
+    private List<Integer> numbers;
+
+    public void setNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
