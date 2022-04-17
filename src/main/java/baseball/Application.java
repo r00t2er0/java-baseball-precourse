2d8501cbeb2e2f0c41d4a2a4352bcd7dfc985526
hermes9003 <hermes9003@ariatech.kr>
2022-04-17 20:58:26 +0900

    feat: add input function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import baseball.model.Computer;
+import baseball.model.Player;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Player player = new Player();
+        Computer computer = new Computer();
+
+        while(true) {
+            System.out.printf("숫자를 입력해주세요: ");
+            player.setNumber(Console.readLine());
+        }
     }
 }
