e2786cbecfdbd2d0bf54fb82f176dc42e575e3e5
hermes9003 <hermes9003@ariatech.kr>
2022-04-17 18:46:12 +0900

    feat: update GameNumber object
    Change function of GameNumber:
    - add constructor
    - modify setNumber function
    
    BREAKING CHANGE: initalize number value
    using random library when creating object

--- src/main/java/baseball/model/GameNumber.java
+++ src/main/java/baseball/model/GameNumber.java
@@ -1,12 +1,23 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class GameNumber {
     private List<Integer> numbers;
 
-    public void setNumbers(List<Integer> numbers) {
+    public GameNumber() {
+        this.numbers = Randoms.pickUniqueNumbersInRange(0,9,3);
+    }
+
+    public GameNumber(List<Integer> numbers) {
         this.numbers = numbers;
+    }
+
+    public void setNumbers(String[] numbers) {
+        for(String number : numbers) {
+            this.numbers.add(Integer.parseInt(number));
+        }
     }
 
     public List<Integer> getNumbers() {
