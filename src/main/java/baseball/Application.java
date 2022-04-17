0612c8a62d77a136cc9558f6ac9b5336486e9ff1
hermes9003 <hermes9003@ariatech.kr>
2022-04-17 21:11:14 +0900

    feat: add end, restart function
    Add select restart or end function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -35,6 +35,17 @@ public class Application {
             }
 
             player.initGameNumber();
+
+            if(strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int selectNumber = Integer.parseInt(Console.readLine());
+                if(selectNumber == 2) {
+                    break;
+                } else {
+                    computer = new Computer();
+                }
+            }
         }
     }
 }
