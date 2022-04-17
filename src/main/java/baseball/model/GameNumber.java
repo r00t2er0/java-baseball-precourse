cb3f734d14c50d032794a393a63fdf27ec9457f7
hermes9003 <hermes9003@ariatech.kr>
2022-04-17 20:54:26 +0900

    refactor: random function replace
    From pickUniqueNumbersInRange to pickNumberInRange

--- src/main/java/baseball/model/GameNumber.java
+++ src/main/java/baseball/model/GameNumber.java
@@ -1,18 +1,33 @@
 package baseball.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameNumber {
     private final static int NUMBER_SIZE = 3;
+    private final static int MIN_RANGE = 0;
+    private final static int MAX_RANGE = 9;
     private List<Integer> numbers;
 
     public GameNumber() {
-        this.numbers = Randoms.pickUniqueNumbersInRange(0,9,3);
+        this.numbers = pickNumbers();
     }
 
     public GameNumber(List<Integer> numbers) {
         this.numbers = numbers;
+    }
+
+    public List<Integer> pickNumbers() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while(randomNumbers.size() < NUMBER_SIZE) {
+            int number = Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE);
+            if(!randomNumbers.contains(number)) {
+                randomNumbers.add(number);
+            }
+        }
+        return randomNumbers;
     }
 
     public void setNumbers(String inputNumber) {
