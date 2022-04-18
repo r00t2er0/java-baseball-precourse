464915768a90b117d88198420dc095655ea7c3f9
hermes9003 <hermes9003@ariatech.kr>
2022-04-18 14:29:55 +0900

    feat: initGameNumber function
    Add initGameNumber in Computer object

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -6,6 +6,10 @@ public class Computer extends Player{
     private GameNumber gameNumber;
 
     public Computer() {
+        initGameNumber();
+    }
+
+    public void initGameNumber() {
         this.gameNumber = new GameNumber();
     }
 
